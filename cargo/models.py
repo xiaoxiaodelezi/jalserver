a1a94b9c78ad07307737e4ac3d44ec55d90f7b4b
xiaoxiaodelezi <eachdayachance@hotmail.com>
2022-12-01 19:08:47 +0800

    alpha 1.1.0

--- cargo/models.py
+++ cargo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Airport(models.Model):
+    station=models.CharField(max_length=3)
+    country=models.CharField(max_length=2)
+    def __str__(self):
+        return self.station
